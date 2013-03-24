37915ebada1c17f432f0c4a47be03836a8e57ecd
Melissa Choy <mel@otilas.com.au>
2013-03-25 10:02:25 +1100

    fix typo

--- lib/radiant-cells-extension.rb
+++ lib/radiant-cells-extension.rb
@@ -1,7 +1,7 @@
 module RadiantCellsExtension
   VERSION     = "1.0.0"
   SUMMARY     = "Cells for Radiant CMS"
-  DESCRIPTION = "Allows of the cells gem in your radiant site and extensions"
+  DESCRIPTION = "Allows use of the cells gem in your radiant site and extensions"
   URL         = "https://github.com/melchoy/radiant-cells-extension"
   AUTHORS     = ["Melissa Choy"]
   EMAIL       = ["mel@otilas.com.au"]
